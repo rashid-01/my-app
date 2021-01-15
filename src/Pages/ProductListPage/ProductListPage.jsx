beb12d55da256de34ce1bdf9e98add29f36889f2
rashid-01 <mdrashid.1000@gmail.com>
2021-01-15 23:25:59 +0530

    chnages

--- src/Pages/ProductListPage/ProductListPage.jsx
+++ src/Pages/ProductListPage/ProductListPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 import AppCategoryTab from '../../Components/Layout/AppCategoryTab';
 import AppProductList from '../../Components/Layout/AppProductList';
 import useProductListPage from './useProductListPage';
@@ -6,11 +7,12 @@ import useProductListPage from './useProductListPage';
 function ProductListPage() {
     const { categoryData, pageHeading, getProductList, productData } = useProductListPage()
     return (
-        <div>
+        <Container>
             <h4>{pageHeading}</h4>
-            <AppCategoryTab list={categoryData} onPress={getProductList}/>
+            <AppCategoryTab list={categoryData} onPress={getProductList} />
+            <button type="button" class="btn btn-sm btn-primary" id="rotate" >Need Help</button>
             <AppProductList productListData={productData}/>
-        </div>
+        </Container>
     )
 }
 
